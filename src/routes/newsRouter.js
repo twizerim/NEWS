0c3f40cb2dc213c245961d329daf5dcc81ba97a5
hp <emmanueldusengimana83@gmail.com>
2023-10-03 09:31:53 +0200

    add

--- src/routes/newsRouter.js
+++ src/routes/newsRouter.js
@@ -5,13 +5,13 @@ import verifyAccess from "../middleware/velifyAccess"
 
 
 const router=express.Router()
-router.post("/news",NewsController.createnews)  
+router.post("/news",verifyAccess("admin"),NewsController.createnews)  
 router.get("/news",NewsController.getAllnews)
 router.patch("/:id",verifyAccess("admin"),NewsController.updatenews)
-router.delete("/:id",verifyAccess("admin"),NewsController.deleteOnenews)
+router.delete("/:id",NewsController.deleteOnenews)
 router.put("/like/:id",verifyAccess("user"),NewsController.likes)
 router.put("/dislike/:id",verifyAccess("user"),NewsController.dislikes)
-router.delete("/",NewsController.deleteAll)
+router.delete("/",verifyAccess("admin"),NewsController.deleteAll)
 router.get("/search",NewsController.searchCategory)
 
 
