c5e8b55f821b4662b76df9e4e442328a776f027f
hp <emmanueldusengimana83@gmail.com>
2023-10-24 11:28:12 +0200

    ggy

--- src/middleware/velifyAccess.js
+++ src/middleware/velifyAccess.js
@@ -18,38 +18,11 @@ const VerifyAccess = (passRole)=>{
         return next()
       }
      } catch (error) {
-      
+      if ((error.name = "JsonWebTokenError"))
+          return errorRisponse(res, 401, "Invalid Token or Expired Token");
      }
     }
   }
 
 }
 export default VerifyAccess
-
-// const VerifyAccess = (passRole) => {
-//   return (req, res, next) => {
-//     //set token in header
-//     const token = req.headers["auth-token"];
-//     if (!token) {
-//       return errorRisponse(res, 401, `No token provided`);
-//     } else {
-//       try {
-//         const verifyToken = jwt.verify(token, process.env.SECRET_KEY, {
-//           expiresIn: "1d",
-//         });
-
-//         req.user = verifyToken.user;
-
-//         if (passRole !== verifyToken.user.role) {
-//           return errorRisponse(res, 401, `You don't have access`);
-//         } else {
-//           return next();
-//         }
-//       } catch (error) {
-//         if ((error.name = "JsonWebTokenError"))
-//           return errorRisponse(res, 401, "Invalid Token or Expired Token");
-//       }
-//     }
-//   };
-// };
-// export default VerifyAccess;
