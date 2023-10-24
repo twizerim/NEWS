0bd9839263bb073b9aee50c1716fa57aa6227d95
hp <emmanueldusengimana83@gmail.com>
2023-10-24 11:23:34 +0200

    ggy

--- src/middleware/velifyAccess.js
+++ src/middleware/velifyAccess.js
@@ -8,6 +8,18 @@ const VerifyAccess = (passRole)=>{
     const token = req.headers["auth-token"]
     if(!token){
       return errorRisponse(res,401,`no token provided`)
+    }else{
+     try {
+      const verifyToken = jwt.verify(token,process.env.SCRET_KEY,{expiresIn:"2d"})
+      req.user=verifyToken.user
+      if(passRole !== verifyToken.user.role){
+        return errorRisponse(res,401,`please you have not access`)
+      }else{
+        return next()
+      }
+     } catch (error) {
+      
+     }
     }
   }
 
